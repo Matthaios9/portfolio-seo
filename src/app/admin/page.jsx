1d1666f3c13be19a5b57667744076b0501bce193
Goutham Reddy <gouthamsumitreddy09@gmail.com>
2023-11-01 20:04:39 +0530

    user analytics

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -44,6 +44,6 @@ export default async function Home() {
 export const dynamic = 'force-dynamic'; 
 
 export const metadata = {
-  title: 'Issue Tracker - Dashboard',
-  description: 'View a summary of project issues'
+  title: 'Mattheos Tasios - Dashboard',
+  description: 'User Analytics'
 };
