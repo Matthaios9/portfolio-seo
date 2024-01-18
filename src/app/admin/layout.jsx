31643ac12f0fc1fc8f814ddc9662db329101bf59
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-18 23:05:44 +0530

    loading removed layout added

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { ReactNode, useEffect } from "react";
+
+import AuthProvider from "../auth/Provider";
+import { Theme, Container } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import AuthNavBar from "./AuthNavBar";
+import { usePathname, useRouter } from 'next/navigation'
+
+const AdminLayout = ({ children }) => {
+  const pathname = usePathname()
+  const router = useRouter()
+  useEffect(() => {
+    console.log('path', pathname);
+    if (pathname === '/admin') {
+      router.refresh()
+    }
+
+  }, [])
+  return (
+    <AuthProvider>
+      <Theme>
+        <Container className="h-96">
+          <AuthNavBar />
+          {children}
+        </Container>
+      </Theme>
+    </AuthProvider>
+  );
+};
+
+
+export default AdminLayout;
