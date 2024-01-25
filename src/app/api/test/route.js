9b29a100200586c4661f409663eaf34d696814bd
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-01-25 12:17:34 +0530

    added fix for test email route

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -2,6 +2,10 @@ import { NextResponse } from "next/server";
 import { sendTestEmail } from "../../../helper/mailer";
 
 export async function GET(request) {
-  await sendTestEmail("r3ddy03@gmail.com", "Goutham");
-  return NextResponse.json({ msg: "test" });
+  try {
+    await sendTestEmail("r3ddy03@gmail.com", "Goutham");
+    return NextResponse.json({ msg: "test" });
+  } catch (err) {
+    return NextResponse.json({ errror: err }, { status: 404 });
+  }
 }
