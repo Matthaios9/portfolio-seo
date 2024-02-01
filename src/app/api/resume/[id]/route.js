f56801aa8c216089f3155f5a1bdb91d5994547ce
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-02-01 19:49:03 +0530

    updated with fallsafe

--- src/app/api/resume/[id]/route.js
+++ src/app/api/resume/[id]/route.js
@@ -6,33 +6,37 @@ import { PrismaClient } from "@prisma/client";
 import { sendEmail } from "../../../../helper/mailer";
 
 export async function PATCH(request, { params }) {
-  const prisma = new PrismaClient();
-  const session = await getServerSession(authOptions);
-  const reqBody = await request.json();
-  //   console.log(reqBody); //for debuging
-  if (!session) return NextResponse.json({}, { status: 401 });
-  const user = await prisma.user.findUnique({
-    where: {
-      id: params.id,
-    },
-  });
+  try {
+    const prisma = new PrismaClient();
+    const session = await getServerSession(authOptions);
+    const reqBody = await request.json();
+    //   console.log(reqBody); //for debuging
+    if (!session) return NextResponse.json({}, { status: 401 });
+    const user = await prisma.user.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
 
-  if (!user)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  const updateResumeRequestStatus = await prisma.user.update({
-    where: { id: params.id },
-    data: {
-      resume_requested: reqBody.status,
-    },
-  });
-  if (updateResumeRequestStatus.resume_requested === "APPROVED") {
-    await sendEmail(user.email, user.name); // mail not working
-    return true;
+    const updateResumeRequestStatus = await prisma.user.update({
+      where: { id: params.id },
+      data: {
+        resume_requested: reqBody.status,
+      },
+    });
+    if (updateResumeRequestStatus.resume_requested === "APPROVED") {
+      await sendEmail(user.email, user.name); // mail not working
+      return true;
+    }
+
+    return NextResponse.json(
+      { status: updateResumeRequestStatus },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ errror: err }, { status: 404 });
   }
-
-  return NextResponse.json(
-    { status: updateResumeRequestStatus },
-    { status: 200 }
-  );
 }
