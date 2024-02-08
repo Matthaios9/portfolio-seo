13acf972811c25341522d1d0538b323134f3119b
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-02-08 20:37:42 +0530

    updated

--- src/helper/mailer.js
+++ src/helper/mailer.js
@@ -65,7 +65,7 @@ export const sendEmail = async (email, name, file) => {
     const base_url = process.env.NEXTAUTH_URL;
     console.log("file ", file);
     const pdfUrl = file; //base_url + `/assets/Mattheos_Tasios_-_Junior_Software_Developer.pdf`;
-    const defaultEmaiemplate = `<p>Hi, Congratulation your request for Resume approved on <a href="${base_url}" traget="_blank"">zutet.com</a>. Your can signin to <a href="${pdfUrl}">download</a> my resume</p>`;
+    const defaultEmaiemplate = `<p>Hi, Congratulation your request for Resume approved on <a href="${base_url}" traget="_blank"">zutet.com</a>. Your can signin to my resume</p>`;
     const options = {
       from: "dev@zutet.com",
       to: email,
