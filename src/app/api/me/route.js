cd591516be282e1fce52d0aef85c9d39d958840b
G Goutham Reddy <goutamsumitreddy09@gmail.com>
2024-04-02 15:54:14 +0530

    updated

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -5,19 +5,29 @@ import authOptions from "../auth/[...nextauth]/authOption";
 import { PrismaClient } from "@prisma/client";
 
 export async function POST(request) {
-  const session = await getServerSession(authOptions);
-  const prisma = new PrismaClient();
-  const resBody = await request.json();
+  try {
+    const session = await getServerSession(authOptions);
+    if (session) {
+      const prisma = new PrismaClient();
+      const resBody = await request.json();
 
-  if (!session) return NextResponse.json({}, { status: 401 });
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session.user.email,
-    },
-  });
+      if (!session) return NextResponse.json({}, { status: 401 });
+      const user = await prisma.user.findUnique({
+        where: {
+          email: session.user.email,
+        },
+      });
 
-  return NextResponse.json({
-    mesaaage: "User found",
-    data: user,
-  });
+      return NextResponse.json({
+        mesaaage: "User found",
+        data: user,
+      });
+    }
+    return NextResponse.json({
+      mesaaage: "No found",
+      data: [],
+    });
+  } catch (err) {
+    return NextResponse.json({ errror: err }, { status: 404 });
+  }
 }
