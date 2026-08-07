f5329d840088e7a41d307df779e6bf1523e240f0
MominulIslam007 <ronysam68@gmail.com>
2026-08-07 15:00:21 +0600

    Update route.js

--- src/app/api/project/route.js
+++ src/app/api/project/route.js
@@ -1,39 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import { projectSchema } from "../../../validationSchema/validationSchema";
+import { NextResponse } from "next/server";
+import data from "../../sections/portfolio/data";
 
 export async function GET(request) {
-  // return NextResponse.json(params);
-  const prisma = new PrismaClient();
-  const project = await prisma.project.findMany();
-  if (!project) return NextResponse.json({}, { status: 401 });
-  return NextResponse.json(project, { status: 200 });
+  return NextResponse.json(data, { status: 200 });
 }
 
 export async function POST(request) {
-  const bodyRes = await request.json();
-  const prisma = new PrismaClient();
-
-  const validation = projectSchema.safeParse(bodyRes);
-  if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  const { title, category, description, projectLink, githubLink, imageId } =
-    bodyRes;
-  //   return NextResponse.json(bodyRes);
-  const newProject = await prisma.project.create({
-    data: {
-      title,
-      category,
-      description,
-      projectLink,
-      githubLink,
-      imageId,
-    },
-  });
-  if (!newProject)
-    return NextResponse.json(
-      { error: "Somthing is went wrong" },
-      { status: 400 }
-    );
-  return NextResponse.json(newProject);
+  return NextResponse.json({ error: "Database not available" }, { status: 503 });
 }
