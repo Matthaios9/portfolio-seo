b73cdbe8a045fba31a58f56e3dc21c1fd8ed9af1
MominulIslam007 <ronysam68@gmail.com>
2026-08-10 11:16:03 +0600

    Update data.js

--- src/app/sections/portfolio/data.js
+++ src/app/sections/portfolio/data.js
@@ -54,7 +54,7 @@ const data = [
     category: "Cybersecurity",
     description:
       "Analyzed network packet capture files (.pcap) using Zeek logs and Suricata to trace malicious command-and-control (C2) traffic, DNS tunneling, and data exfiltration patterns.",
-    projectLink: "https://www.malware-traffic-analysis.net/",
+    projectLink: "https://1drv.ms/v/c/8ba7733f57b4cf84/IQDZKMMqlRHhTrOStzTbl3QeAcCJjlP32WnX8MXjZDH2wu4?e=KIhDsF",
     githubLink: "https://github.com/brimdata/brim",
     imageId:
       "https://images.unsplash.com/photo-1551808525-51a94da548ce?w=300&h=150&fit=crop",
